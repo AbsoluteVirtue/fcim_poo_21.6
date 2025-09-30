f28e53bf0a6b20a78ebf53bde1b3d75407f5901c
AbsoluteVirtue <3hitemouse@gmail.com>
2025-09-30 11:15:40 +0300

    Update pr06.cpp

--- pr06.cpp
+++ pr06.cpp
@@ -1,24 +1,65 @@
+// Implementation inheritance
+struct A {
+    void f() {}
+};
 
-struct interface // superclass
+struct B : public A {
+    void f() {}
+};
+
+struct C : public A {
+    void f() {}
+};
+
+// Interface inheritance
+struct Interface // superclass
 {
     virtual void f() = 0;
     virtual void g() = 0;
 };
 
-struct impl {
+struct Impl {
     void f() {}
     void g() {}
 };
 
-struct a : public interface, public impl {
-    void f() override { impl::f(); }
-    void g() override { impl::g(); }
+struct Adaptor : public Interface, public Impl {
+    void f() override { Impl::f(); }
+    void g() override { Impl::g(); }
 };
 
+// Virtual inheritance
+struct Top // superclass
+{
+    char c;
+};
+
+struct MidA : virtual Top
+{
+    char c;
+};
+
+struct MidB : virtual Top
+{
+    char c;
+};
+
+class Bot : MidA, MidB
+{
+    char c;
+public:
+    void f()
+    {
+        c + Top::c + MidA::c + MidB::c;
+    }
+};
+
+void var (A& arg) {
+    arg.f(); // ?
+}
+
 int main() {
-
-    a x;
-    x.f();
-    x.g();
-
+    A a; var(a);
+    B b; var(b);
+    C c; var(c);
 }
