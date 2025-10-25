a1f8e48e06883263eeabdf3f6dc0452cd49c28f9
AbsoluteVirtue <3hitemouse@gmail.com>
2025-10-25 14:16:19 +0300

    tweaks

--- pr09.cpp
+++ pr09.cpp
@@ -3,28 +3,31 @@
 #include <cassert>
 #include <initializer_list>
 #include <iostream>
+#include <iterator>
 
 class var
 {
 private:
     size_t _size{0};
-    int *_data{nullptr};
+    int*   _data{nullptr};
 
 public:
+    size_t size() const { return _size; }
+    const int *data() const { return _data; }
+
     class iterator {
         int *p{nullptr};
     public:
         using value_type = int;
         using difference_type = std::ptrdiff_t;
+        using iterator_category = std::contiguous_iterator_tag;
 
         iterator() = delete;
         iterator(int* o) : p(o) {}
         iterator(const iterator& o) : iterator(o.p) {}
         ~iterator() = default;
 
-        value_type& operator*() {
-            return *p;
-        }
+        value_type& operator*() { return *p; }
         value_type operator*() const { return *p; }
         friend bool operator==(const iterator& a, const iterator& b) {
             return *a == *b;
@@ -46,35 +49,20 @@ public:
         }
     };
 
-    size_t size() const
-    {
-        return _size;
-    }
-    const int *data() const
-    {
-        return _data;
-    }
+    using reverse_iterator = std::reverse_iterator<iterator>;
 
     var() = default;
-    var(size_t n) : _size{n}
-    {
-        _data = new int[n];
-    }
-    ~var()
-    {
-        delete[] _data;
-    }
+    var(size_t n) : _size{n} { _data = new int[n]; }
+    ~var() { delete[] _data; }
     var(int *p, size_t n)
     {
         _size = n;
         _data = p;
     }
-    var(const std::initializer_list<int> &l) : var(l.size())
-    {
+    var(const std::initializer_list<int> &l) : var(l.size()) {
         std::copy(l.begin(), l.end(), _data);
     }
-    var(const var &a)
-    {
+    var(const var &a) {
         _size = a.size();
         _data = new int[_size];
         for (size_t i = 0; i < size(); i++)
@@ -82,55 +70,42 @@ public:
             _data[i] = a.data()[i];
         }
     }
-    var &operator=(const var &o)
-    {
+    var &operator=(const var &o) {
         var copy{o};
         std::swap(copy._data, _data);
         std::swap(copy._size, _size);
         return (*this);
     }
-    var(var &&m)
-    {
+    var(var &&m) {
         _data = m._data;
         _size = m._size;
 
         m._data = nullptr;
         m._size = 0;
     }
-    var &operator=(var &&m)
-    {
+    var &operator=(var &&m) {
         var copy{std::move(m)};
         std::swap(copy._data, _data);
         std::swap(copy._size, _size);
         return *this;
     }
-    auto begin() {
-        return iterator(_data);
-    }
-    auto cbegin() const {
-        return iterator(_data);
-    }
-    auto end() {
-        return iterator(_data + size());
-    }
-    auto cend() const {
-        return iterator(_data + size());
-    }
-
-    bool operator==(const var &r)
-    {
+    auto begin() { return iterator(_data); }
+    auto cbegin() const { return iterator(_data); }
+    auto rbegin() { return reverse_iterator(end()); }
+    auto end() { return iterator(_data + size()); }
+    auto cend() const { return iterator(_data + size()); }
+    auto rend() {return reverse_iterator(begin()); }
+    bool operator==(const var &r) {
         auto first1 = cbegin();
         auto last1 = cend();
         auto first2 = r.cbegin();
         auto last2 = r.cend();
         return std::equal(first1, last1, first2, last2);
     }
-    bool operator!=(const var &r)
-    {
+    bool operator!=(const var &r) {
         return !(*this == r);
     }
-    friend inline bool operator<(const var &l, const var &r)
-    {
+    friend inline bool operator<(const var &l, const var &r) {
         auto first1 = l.cbegin();
         auto last1 = l.cend();
         auto first2 = r.cbegin();
@@ -140,8 +115,7 @@ public:
     friend inline bool operator>(const var &lhs, const var &rhs) { return rhs < lhs; }
     friend inline bool operator<=(const var &lhs, const var &rhs) { return !(lhs > rhs); }
     friend inline bool operator>=(const var &lhs, const var &rhs) { return !(lhs < rhs); }
-    friend std::ostream& operator<<(std::ostream& os, const var& obj)
-    {
+    friend std::ostream& operator<<(std::ostream& os, const var& obj) {
         for(size_t i=0; i < obj.size(); ++i)
             os << obj[i];
         return os;
@@ -168,5 +142,6 @@ int main()
     var a{1, 2, 3, 4 ,5};
     var b{2, 3, 4 ,5};
     
-    std::cout << ((a < b) ? "true" : "false");
+    std::cout << ((a <  b) ? "true" : "false");
+    std::cout << ((a == b) ? "true" : "false");
 }
